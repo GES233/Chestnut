4d8df70507efe7d85aecd329163f5a0e6d72291c
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-10 12:32:20 +0800

    ✅ test: 添加了领域模型测试

--- tests/app/test_document.py
+++ tests/app/test_document.py
@@ -6,6 +6,9 @@ from pathlib import Path
 from tinyui.application.document.domain.document import Document
 from tinyui.application.document.domain.meta import DocumentMeta
 from tinyui.application.document.domain.repo import DocRepo, DocMetaRepo
+from tinyui.application.document.dto.load import DocumentLoader
+from tinyui.application.document.dto.present import DocumentPresenter
+from tinyui.infrastructure.helpers.path import INSTANCE_PATH
 
 
 DOCUMENT_RAW_CONTENT = """# 只因的美学
@@ -25,7 +28,7 @@ by 呕像恋蜥僧
 
 
 class TestDocumentDomain:
-    
+
     def test_document_meta(self) -> None:
         if title_ := re.match(r"^# (.*)\n", DOCUMENT_RAW_CONTENT, re.MULTILINE):
             title = title_.group(1)
@@ -49,17 +52,30 @@ class TestDocumentDomain:
             title = title_.group(1)
         else:
             title = None
-        """
-        demo_doc = Document(
+
+        demo_meta = DocumentMeta(
             name="awesome_chicken",
             # from tinyui/infra/deps/document/dir/build_index
             title=title,
+            language="cmn-Hans",
             source=Path(__file__),
             location=["wild", "breeding", "chicken"],
             categories=[],
-            content=DOCUMENT_RAW_CONTENT
+            # content=DOCUMENT_RAW_CONTENT
         )
 
-        assert demo_doc.name == "awesome_chicken"
-        assert demo_doc.title == "只因的美学"
-"""
+        demo = Document(
+            id=demo_meta.name,
+            meta=demo_meta,
+            content=DOCUMENT_RAW_CONTENT,
+        )
+
+        assert demo.meta.name == demo.id
+
+
+class TestDTO:
+    def _store_file(self, path: Path, content: str) -> None:
+        pass
+
+    def test_file_load(self) -> None:
+        ...
