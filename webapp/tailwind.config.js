3f5d7c64ab67dd82845d03672092498d8f49ee27
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-10 20:28:35 +0800

    💄 style: 添加了配色方案

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // From https://www.happyhues.co/palettes/16
+        'tiny-light-fir': '#fffffe',
+        'tiny-light-sec': '#fff3ec',
+        'tiny-chestnut': '#55423d',
+        'tiny-dark-sec': '#271c19',
+        'tiny-dark-fir': '#140d0b',
+        'tiny-side-fir': '#e78fb3',
+        'tiny-side-sec': '#ffc0ad',
+        'tiny-side-ter': '#9656a1',
+      },
+    },
   },
   plugins: [],
 }
