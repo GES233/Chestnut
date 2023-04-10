d622859e6eb8120c7b90ef7067e27344a2c7e1c8
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-10 20:52:31 +0800

    💄 style: 尝试了上述变化

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -6,17 +6,17 @@ const state: State = 0;
 const Header = (content) => {
 return (
 <div> {/* Comment for JSX. */}
-  <h1 class="bg-purple-600 bg-opacity-75 text-4xl text-white text-center font-semibold rounded">{content["content"]}</h1>
+  <h1 class="bg-tiny-chestnut bg-opacity-75 text-4xl text-tiny-light-sec text-center font-semibold rounded">{content["content"]}</h1>
   <hr></hr>
 </div>
 )};
 
 const view: View<State> = (state: State) => {
 return (
-<div class="text-center"><Header content="Tiny UI build with Sanic and AppRun"/>
-  <p class="font-mono text-center">Counter: <span class="text-2xl">{state}</span></p>
-  <button class="bg-transparent border border-purple-400 hover:bg-purple-500 bg-opacity-50 rounded-full text-purple-400 font-mono text-xl py-2 px-1" onclick={() => app.run("inc")}>+1</button>
-  <button class="bg-transparent border border-purple-400 hover:bg-purple-500 bg-opacity-50 rounded-full text-purple-400 font-mono text-xl py-2 px-1" onclick={() => app.run("dec")}>-1</button>
+<div class="text-center bg-tiny-dark-sec"><Header content="Tiny UI build with Sanic and AppRun"/>
+  <p class="font-mono text-center text-tiny-light-sec">Counter: <span class="text-2xl">{state}</span></p>
+  <button class="bg-transparent border border-tiny-side-fir hover:bg-tiny-side-sec bg-opacity-50 rounded-full text-tiny-side-ter font-mono text-xl py-2 px-1" onclick={() => app.run("inc")}>+1</button>
+  <button class="bg-transparent border border-tiny-side-fir hover:bg-tiny-side-sec bg-opacity-50 rounded-full text-tiny-side-ter font-mono text-xl py-2 px-1" onclick={() => app.run("dec")}>-1</button>
 </div>
 )};
 
