cee12dc902baa4882e95f0a8ab764720228950bf
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-12 23:58:25 +0800

    💄 style: 想要用新方式来表达颜色

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
         'tiny-side-fir': '#e78fb3',
         'tiny-side-sec': '#ffc0ad',
         'tiny-side-ter': '#9656a1',
+        'tinyui-chestnut': {
+          // Let color into degree.
+          // The author has not basic knowledge of music theory.
+          'tonic': '#55423d',
+          'superleading': '#fff3ec',
+          'subdominant': '#9656a1',
+          'dominant': '#ffc0ad',
+          'submediant': '#e78fb3',
+          'leading': '#140d0b',
+        },
       },
     },
   },
