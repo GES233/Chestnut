e9ad97b9cd48b568f3a105bb819cb7d3c39975ca
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-12 15:15:10 +0800

    🐛 fix: 修复了 `path.INSTANCE_PATH` 的 bug

--- tinyui/infra/helpers/path.py
+++ tinyui/infra/helpers/path.py
@@ -10,10 +10,10 @@ from typing import Callable
 PROJECT_PATH = Path(__file__).cwd()
 BACKEND_PATH = Path(PROJECT_PATH / "tinyui")
 INSTANCE_PATH = Path(PROJECT_PATH / "instance")
-INSTANCE_CONFIG_PATH: Callable[[str], Path] = lambda app_id: Path(
+INSTANCE_CONFIG_PATH: Callable[[str | int | None], Path] = lambda app_id: Path(
     INSTANCE_PATH / "config.toml"
     if not app_id
-    else INSTANCE_PATH / ("config_" + app_id + ".toml")
+    else INSTANCE_PATH / ("config_" + str(app_id) + ".toml")
 )
 DOCS_PATH = Path(PROJECT_PATH / "docs")
 DOCS_ASSETS_PATH = Path(DOCS_PATH / "assets")
