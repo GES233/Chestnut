c9e50a66449496c67e7c247e9e45b4c764af1d75
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-13 15:40:13 +0800

    👌 perf: 添加了 MOTD 的优化

--- chestnut/infra/cmd/motd.py
+++ chestnut/infra/cmd/motd.py
@@ -1,8 +1,12 @@
 """To replace Sanic's native MOTD."""
 import os, sys
-# from shutil import get_terminal_size
+from datetime import datetime, date
+from shutil import get_terminal_size
+from abc import ABC, abstractmethod
 import sanic
 from typing import Any, Dict, Tuple
+
+from ..helpers.config.app import AppConfig
 
 
 # sanic.compat
@@ -10,20 +14,36 @@ def is_atty() -> bool:
     return bool(sys.stdout and sys.stdout.isatty())
 
 
-LOGO = """
+CHESTNUT_ICON = """
 ╔═╗┬ ┬┌─┐┌─┐┌┬┐┌┐┌┬ ┬┌┬┐
 ║  ├─┤├┤ └─┐ │ ││││ │ │ 
-╚═╝┴ ┴└─┘└─┘ ┴ ┘└┘└─┘ ┴ 
+╚═╝┴ ┴└─┘└─┘ ┴ ┘└┘└─┘ ┴  
+
+       🌰🥥🍩🍈
 """
 
 
-class TinyMOTD:
+NAIVE_LOGO = """
+   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄
+   █      █▄▄█      █
+   █▄▄▄▄▄▄█  █▄▄▄▄▄▄█
+          ▄  ▄
+
+ keep simple, keep naive
+
+"""
+
+
+class TinyMOTD(ABC):
     """"""
 
     icon: str | None
     mode: str
+    current: date | datetime
     server_location: str
+    app_config: AppConfig
     version: Dict[str, str | Tuple[int]]
+    system_info: Dict[str, str | Tuple[int]]
     extra: Dict[str, Any] | None
 
     def __init__(
@@ -32,25 +52,57 @@ class TinyMOTD:
         icon: str | None,
         mode: str,
         server_location: str,
-        versions: Dict[str, Any] = {},
+        app_config: AppConfig,
+        version: Dict[str, Any] = {},
+        # TODO: Load prerequisite here.
+        chestnut: bool = False,
+        naive: bool = False,
         **extra
     ) -> None:
-        if icon:
-            self.icon = icon
+        if chestnut == True and naive == True:
+            raise AttributeError
+
+        if naive:
+            self.icon = NAIVE_LOGO
+        elif chestnut:
+            self.icon = CHESTNUT_ICON
+        else:
+            if icon:
+                self.icon = icon
+
         self.mode = mode
+        self.current = datetime.now()
         self.server_location = server_location
-        self.version = dict(sanic=sanic.__version__, **versions)
+        self.app_config = app_config
+        self.version = dict(sanic=sanic.__version__, **version)
+        self.system_info = dict(platform=sys.platform, version=sys.version)
         self.extra = extra
 
+    @abstractmethod
     def display(self) -> None:
-        ...
+        raise NotImplementedError
 
     @classmethod
     def show(cls, *args, **kwds) -> None:
-        class_ = cls(*args, **kwds)
+        class_ = SimpleMOTD(*args, **kwds) if is_atty() else cls(*args, **kwds)
         class_.display()
 
 
 class SimpleMOTD(TinyMOTD):
+    def display(self) -> None:
+        # TODO:
+        # [LOGO or ICON]
+        # [Welcome]
+        # <versions>
+        #   <system and device info>
+        #   <python versions>
+        #   <dependent items>
+        #     - framework...
+        #     - front-end settings...
+        # <information in AppConfig(loaded from instance)>
+        ...
+
+
+class ColorfulMOTD(TinyMOTD):
     def display(self) -> None:
         ...
