b900bf88154708cf54f37a1b48d833ae5f80f1cd
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-13 20:15:36 +0800

    💄 style: 更新了代码

--- chestnut/infra/cmd/motd.py
+++ chestnut/infra/cmd/motd.py
@@ -7,27 +7,9 @@ import sanic
 from typing import Any, Dict, Tuple
 
 from ..helpers.config.app import AppConfig
+from ..helpers.logo import sizecalc
 from ..helpers.utils import is_atty
-
-
-CHESTNUT_ICON = """
-╔═╗┬ ┬┌─┐┌─┐┌┬┐┌┐┌┬ ┬┌┬┐
-║  ├─┤├┤ └─┐ │ ││││ │ │ 
-╚═╝┴ ┴└─┘└─┘ ┴ ┘└┘└─┘ ┴  
-
-       🌰🥥🍩🍈
-"""
-
-
-NAIVE_LOGO = """
-   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄
-   █      █▄▄█      █
-   █▄▄▄▄▄▄█  █▄▄▄▄▄▄█
-          ▄  ▄
-
- keep simple, keep naive
-
-"""
+from ...__version__ import __version__ as chestnut_version
 
 
 class TinyMOTD(ABC):
@@ -51,7 +33,7 @@ class TinyMOTD(ABC):
         app_config: AppConfig,
         version: Dict[str, Any] = {},
         # TODO: Load prerequisite here.
-        chestnut: bool = False,
+        chestnut: bool = True,
         naive: bool = False,
         **extra
     ) -> None:
@@ -59,9 +41,14 @@ class TinyMOTD(ABC):
             raise AttributeError
 
         if naive:
-            self.icon = NAIVE_LOGO
+            from ..helpers.logo import ICON_NAIVE
+
+            self.icon = ICON_NAIVE
         elif chestnut:
-            self.icon = CHESTNUT_ICON
+            # Default.
+            from ..helpers.logo import CHESTNUT_ICON_MIDIAN
+
+            self.icon = CHESTNUT_ICON_MIDIAN
         else:
             if icon:
                 self.icon = icon
@@ -70,7 +57,9 @@ class TinyMOTD(ABC):
         self.current = datetime.now()
         self.server_location = server_location
         self.app_config = app_config
-        self.version = dict(sanic=sanic.__version__, **version)
+        self.version = dict(
+            chestnut=chestnut_version, sanic=sanic.__version__, **version
+        )
         self.system_info = dict(platform=sys.platform, version=sys.version)
         self.extra = extra
 
@@ -80,11 +69,35 @@ class TinyMOTD(ABC):
 
     @classmethod
     def show(cls, *args, **kwds) -> None:
-        class_ = SimpleMOTD(*args, **kwds) if is_atty() else cls(*args, **kwds)
+        class_ = SimpleMOTD(*args, **kwds) if not is_atty() else cls(*args, **kwds)
         class_.display()
 
 
 class SimpleMOTD(TinyMOTD):
+    def __init__(
+        self,
+        *,
+        icon: str | None,
+        mode: str,
+        server_location: str,
+        app_config: AppConfig,
+        version: Dict[str, Any] = {},
+        chestnut: bool = False,
+        naive: bool = False,
+        **extra
+    ) -> None:
+        super().__init__(
+            icon=icon,
+            mode=mode,
+            serverlocation=server_location,
+            app_config=app_config,
+            version=version,
+            chestnut=chestnut,
+            naive=naive,
+            **extra
+        )
+        # TODO: Update icon with size.
+
     def display(self) -> None:
         # TODO:
         # [LOGO or ICON]
@@ -101,4 +114,7 @@ class SimpleMOTD(TinyMOTD):
 
 class ColorfulMOTD(TinyMOTD):
     def display(self) -> None:
+        ...
+    
+    def _parse_values(self) -> Dict[str, Any]:
         ...
---
+++ chestnut/infra/helpers/logo.py
@@ -0,0 +1,42 @@
+"""ASCII Art of Chestnut."""
+from typing import Any, Tuple
+
+
+CHESTNUT_ICON_SMALL =  """🌰 with 🥥, 🍩 and 🍈"""
+
+
+CHESTNUT_ICON_MIDIAN = """
+╔═╗┬ ┬┌─┐┌─┐┌┬┐┌┐┌┬ ┬┌┬┐
+║  ├─┤├┤ └─┐ │ ││││ │ │ 
+╚═╝┴ ┴└─┘└─┘ ┴ ┘└┘└─┘ ┴ 
+"""
+
+
+CHESTNUT_ICON_LARGE = """
+"""
+
+
+ICON_NAIVE = """
+   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄
+   █      █▄▄█      █
+   █▄▄▄▄▄▄█  █▄▄▄▄▄▄█
+          ▄  ▄
+
+ keep simple, keep naive
+"""
+"+1s"
+
+
+def sizecalc(logo: str) -> Tuple[int, int]:
+    """Return the size of ascii art ilustration as (raw, column)."""
+
+    lines = logo.split("\n")
+    
+    column = 0
+    for line in lines:
+        if len(line) > column:
+            column = len(line)
+
+    raw = len(lines)
+
+    return raw, column
