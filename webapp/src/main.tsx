fd33bd18dc2048566ec8211680a22dc18492b042
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-13 10:17:14 +0800

    💄 style: 更新了配色方案的格式

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -3,20 +3,13 @@ import app, { View, Update } from 'apprun';
 type State = number;
 const state: State = 0;
 
-const Header = (content) => {
-return (
-<div> {/* Comment for JSX. */}
-  <h1 class="bg-tiny-chestnut bg-opacity-75 text-4xl text-tiny-light-sec text-center font-semibold rounded">{content["content"]}</h1>
-  <hr></hr>
-</div>
-)};
 
 const view: View<State> = (state: State) => {
 return (
-<div class="text-center bg-tiny-dark-sec"><Header content="TinyUI build with Sanic and AppRun"/>
-  <p class="font-mono text-center text-tiny-light-sec">Counter: <span class="text-2xl">{state}</span></p>
-  <button class="bg-transparent border border-tiny-side-fir hover:bg-tiny-side-sec bg-opacity-50 rounded-full text-tiny-side-ter font-mono text-xl py-2 px-1" onclick={() => app.run("inc")}>+1</button>
-  <button class="bg-transparent border border-tiny-side-fir hover:bg-tiny-side-sec bg-opacity-50 rounded-full text-tiny-side-ter font-mono text-xl py-2 px-1" onclick={() => app.run("dec")}>-1</button>
+<div class="text-center bg-tinyui-chestnut-leading">
+  <p class="font-mono text-center bg-tinyui-chestnut-superleading">Counter: <span class="text-2xl">{state}</span></p>
+  <button class="bg-transparent border border-tinyui-chestnut-submediant hover:bg-tinyui-chestnut-dominant bg-opacity-50 rounded-full text-bg-tinyui-chestnut-subdominant font-mono text-xl py-2 px-1" onclick={() => app.run("inc")}>+1</button>
+  <button class="bg-transparent border border-tinyui-chestnut-submediant hover:bg-tinyui-chestnut-dominant bg-opacity-50 rounded-full text-tinyui-chestnut-subdominant font-mono text-xl py-2 px-1" onclick={() => app.run("dec")}>-1</button>
 </div>
 )};
 
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -8,14 +8,6 @@ module.exports = {
     extend: {
       colors: {
         // From https://www.happyhues.co/palettes/16
-        'tiny-light-fir': '#fffffe',
-        'tiny-light-sec': '#fff3ec',
-        'tiny-chestnut': '#55423d',
-        'tiny-dark-sec': '#271c19',
-        'tiny-dark-fir': '#140d0b',
-        'tiny-side-fir': '#e78fb3',
-        'tiny-side-sec': '#ffc0ad',
-        'tiny-side-ter': '#9656a1',
         'tinyui-chestnut': {
           // Let color into degree.
           // The author has not basic knowledge of music theory.
