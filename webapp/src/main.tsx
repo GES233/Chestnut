52c4e50c0e1473f862eff9a424543615caf875c6
RoastChestnut <chestnut.roasted@outlook.com>
2023-04-14 10:36:30 +0800

    💄 style: 修改 UI ，试图确定配色方案

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -6,7 +6,8 @@ const state: State = 0;
 
 const view: View<State> = (state: State) => {
 return (
-<div class="text-center bg-chestnut-ui-leading">
+<div class="bg-chestnut-ui-tonic">
+  <h1 class="text-center">Chestnut</h1>
   <p class="font-mono text-center bg-chestnut-ui-superleading">Counter: <span class="text-2xl">{state}</span></p>
   <button class="bg-transparent border border-chestnut-ui-submediant hover:bg-chestnut-ui-dominant bg-opacity-50 rounded-full text-chestnut-ui-subdominant font-mono text-xl py-2 px-1" onclick={() => app.run("inc")}>+1</button>
   <button class="bg-transparent border border-chestnut-ui-submediant hover:bg-chestnut-ui-dominant bg-opacity-50 rounded-full text-chestnut-ui-subdominant font-mono text-xl py-2 px-1" onclick={() => app.run("dec")}>-1</button>
